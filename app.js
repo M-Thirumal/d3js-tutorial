128f0ee83c87815f58e123a34d6961732c2bfeb3
Thirumal <m.thirumal@hotmail.com>
2021-09-08 20:13:45 +0530

    data

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
+const DUMMY_DATA = [
+    {id: "1", value: 10, region: "India"},
+    {id: "2", value: 20, region: "USA"},
+    {id: "3", value: 30, region: "China"},
+    {id: "4", value: 40, region: "Russia"},
+];
+
 d3.select('div')
+    .selectAll('p')
+    .data(DUMMY_DATA)
+    .enter()
+    .append('p')
+    .text(dt => dt.region)
     
